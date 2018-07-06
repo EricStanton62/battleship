287932457010abb312dc06726725cb2a54dceecb
EricStanton <40764581+EricStanton62@users.noreply.github.com>
2018-07-06 15:50:27 -0400

    changed form home cleaned up temp, and fixed a few bugs the mac didn't have

--- battleship.py
+++ battleship.py
@@ -1,13 +1,16 @@
 from random import randint
 
-# I use this several times to merely not return an error if the user is supposed to input an integer but does not.
+# I use this several times to merely not return an error if the user is supposed to input an
+# integer but does not.
 def integer_check(statement):
-  number=raw_input(statement)
+  number=input(statement)
   while number.isdigit() == False:
-    number=raw_input(statement)
+    number=input(statement)
   return int(number)
 
-# Helps create the board, codecademy helped write this. not sure why it's a function since I only need one board so I call them only once each. Maybe they thought 2 player the players would need their own board and possibly place their own ships.
+# Helps create the board, codecademy helped write this. not sure why it's a function since I only
+# need one board so I call them only once each. Maybe they thought 2 player the players would
+# need their own board and possibly place their own ships.
 def random_row(board):
   return randint(1, len(board)-1)
 
@@ -15,16 +18,19 @@ def random_col(board):
   return randint(1, len(board[0])-1)
 
 
-# wrote this initially as a check that player guessed a letter/number combination that could be on the board at its largest size, if not simply have them guess again without losing a turn. It returns their guess column as both an integer and a letter in a string and the guess row.
+# wrote this initially as a check that player guessed a letter/number combination that could
+# be on the board at its largest size, if not simply have them guess again without losing a turn.
+# It returns their guess column as both an integer and a letter in a string and the guess row.
 def player_guess():
   y=0
   while y<1:
-    guesses = raw_input("Guess: (EX:a1 or A1)")
+    guesses = input("Guess: (EX:a1 or A1)")
     if not len(guesses) ==2:
       print ("try again in A1 format")
     else:
       guess= guesses.lower()
-      if not guess in "a1a2a3a4a5a6a7a8a9b1b2b3b4b5b6b7b8b9c1c2c3c4c5c6c7c8c9d1d2d3d4d5d6d7d8d9e1e2e3e4e5e6e7e8e9f1f2f3f4f5f6f7f8f9g1g2g3g4g5g6g7g8g9h1h2h3h4h5h6h7h8h9i1i2i3i4i5i6i7i8i9":
+      if not guess in "a1a2a3a4a5a6a7a8a9b1b2b3b4b5b6b7b8b9c1c2c3c4c5c6c7c8c9d1d2d3d4d5d6d7d8d9\
+      e1e2e3e4e5e6e7e8e9f1f2f3f4f5f6f7f8f9g1g2g3g4g5g6g7g8g9h1h2h3h4h5h6h7h8h9i1i2i3i4i5i6i7i8i9":
         print("try again in A1 format(must be on the board also)")
         print_board(board)
       else:  
@@ -34,7 +40,9 @@ def player_guess():
         guess_col = convert_to_number(x)  
         return guess_row,guess_col,x
 
-# simply asks players how many ships they want on the game board, checks for valid input, generates ships checking they're unique locations, and returns a list with first value a string of ship locations and 2nd value the number of ships
+# simply asks players how many ships they want on the game board, checks for valid input, 
+# generates ships checking they're unique locations, and returns a list with first value
+# a string of ship locations and 2nd value the number of ships
 def how_many_ships(): 
   ship_num="How many ships do you want to sink?(integer)"
   numbers=integer_check(ship_num)
@@ -70,27 +78,28 @@ def convert_to_number(lower_case):
           return 9
 
 # converts a number back into a string letter
-def convert_to_letter(temp_ship_col):
-  if temp_ship_col == 1:
+def convert_to_letter(ship_column):
+  if ship_column == 1:
       return "a"
-  elif temp_ship_col ==2:
+  elif ship_column ==2:
       return "b"
-  elif temp_ship_col ==3:
+  elif ship_column ==3:
       return "c"
-  elif temp_ship_col ==4:
+  elif ship_column ==4:
       return "d"
-  elif temp_ship_col ==5:
+  elif ship_column ==5:
       return "e"
-  elif temp_ship_col ==6:
+  elif ship_column ==6:
       return "f"
-  elif temp_ship_col ==7:
+  elif ship_column ==7:
       return "g"
-  elif temp_ship_col ==8:
+  elif ship_column ==8:
       return "h"
-  elif temp_ship_col ==9:
+  elif ship_column ==9:
       return "i"
 
-# this function removes a successful hit location from the ship location string once a player sinks it.
+# this function removes a successful hit location from the ship location string once a player
+# sinks it.
 def sunk_ship(large,small):
   temp_num= large.find(small)
   if len(large)-2==temp_num:
@@ -99,7 +108,8 @@ def sunk_ship(large,small):
     medium= large[:temp_num]+large[temp_num+2:len(large)]
   return medium
 
-# at the end if the players lose, this function places "S" on the remaining ship locations so they can see where they were. 
+# at the end if the players lose, this function places "S" on the remaining ship locations
+# so they can see where they were. 
 def alive_ship(large):
   medium=large[:2]
   guess_col=convert_to_number(medium[0])
@@ -108,7 +118,8 @@ def alive_ship(large):
   return large[2:]
 
 number_of_rows=1
-#simply asks user how many rows they want the battleship board to have, and keeps asking until it receives a valid input.
+# simply asks user how many rows they want the battleship board to have,
+# and keeps asking until it receives a valid input.
 while number_of_rows <2 or number_of_rows >9:  
   welcome="how many rows/columns(2-9)?(must be a square)"
   number_of_rows= integer_check(welcome)
@@ -127,7 +138,8 @@ def print_board(board):
     print (" ".join(row))
 
 print_board(board)
-# a lot of variables I have to make sure exist before they're called upon.. is it normal to have this many? or is this a sign I need more functions?? classes?? help.
+# a lot of variables I have to make sure exist before they're called upon..
+# is it normal to have this many? or is this a sign I need more functions?? classes?? help.
 check=""
 ship_col=0
 ship_row=0
@@ -137,7 +149,7 @@ player2_turn=100
 guess_row=3
 guess_col=3
 temp=[3,3,""]
-temp2=["",1]
+ship_quantity=["",1]
 guess_statement="How many guesses per player would you like?(integer)"
 # players enter how many guesses they'd like.
 guess_num= integer_check(guess_statement)
@@ -150,7 +162,8 @@ which_player=1
 eric ="content"
 # this is set-up for the game
 while eric != "happy":
-# they say they want 0 guesses or negative guesses I just end the program, my integer check will let those values pass on purpose 
+# they say they want 0 guesses or negative guesses I just end the program,
+# my integer check will let those values pass on purpose 
   if guess_num<=0:
     print ("ok then you lose!")
     break
@@ -184,13 +197,14 @@ while eric != "happy":
     player2_turn=0
 
 # players are asked how many ships they want here      
-  temp2=how_many_ships()
-  check=check+temp2[0]
-  ship=int(temp2[1])
+  ship_quantity=how_many_ships()
+  check=check+ship_quantity[0]
+  ship=int(ship_quantity[1])
 # if ships value is less than 1 end the game  
   if ship<=0:
     print ("That's a great strategy in life, never even try.")
-# if there's no way for both players to lose becuase the board is too small for the guesses/ships end the game 
+# if there's no way for both players to lose becuase the board is too small for the
+# guesses/ships end the game 
   if ship>=number_of_rows**number_of_rows-guess_num*player_num:
     print ("That was too many ships, you fail at life.")
     ship=0
@@ -198,42 +212,42 @@ while eric != "happy":
 
 # playing battleship starts
 while turn <guess_num and ship>0:
-  print check
+  print (check)
 
 # if two players randomize whose turn it is and print to let them know.
   if player_num ==2 and player1_guesses == player2_guesses:
     which_player=randint(1,2)
     if which_player==1:
-      print ("Player 1 "),
+      print ("Player 1 ", end="")
     else:
-      print ("Player 2 "),
+      print ("Player 2 ", end="")
   elif player_num==2 and player1_guesses>player2_guesses:
     which_player=1
-    print ("Player 1 "),
+    print ("Player 1 ", end="")
   elif player_num==2 and player2_guesses>player1_guesses:
     which_player=2
-    print ("Player 2 "),
+    print ("Player 2 ", end="")
 
 # if only one player    
   else:
-    print ("Player 1 "),
+    print ("Player 1 ", end="")
   
-  print ("Guesses remaining: "), guess_num-turn
+  print ("Guesses remaining:",  guess_num-turn)
 
 # player inputs a guess here  
   temp = player_guess()
   guess_row = int(temp[0])  
   guess_col = int(temp[1])
   col_check= str(temp[2])
-  temp_check= col_check +str(guess_row)
+  guess_string= col_check +str(guess_row)
 
 # guess is a hit,this adds "S" to the board and gives that player a point
-  if temp_check in check:
+  if guess_string in check:
     print ("Congratulations! You sank a battleship!")
     board[guess_row][guess_col] = "S" 
     print_board(board)
-    fart= sunk_ship(check,temp_check)
-    check=fart
+    remaining_ship= sunk_ship(check,guess_string)
+    check=remaining_ship
     ship-=1
     print (str(ship) +" ship(s) remaining")
     if which_player ==1:
@@ -278,12 +292,12 @@ while turn <guess_num and ship>0:
           print ("Player 2 wins!")
       
       while len(check)>0:
-        farts=alive_ship(check)
-        check=farts
+        remaining_ships=alive_ship(check)
+        check=remaining_ships
       print_board(board)
 
 # just a joke since the program doesn't restart right now. is that easy to do?      
-      joke = str(raw_input("Care to play again?(y/n)?"))
+      joke = str(input("Care to play again?(y/n)?"))
       if joke.lower() == "y":
         print ("Then click run again")
       elif joke.lower() == "n":

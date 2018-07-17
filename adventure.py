697bb2d6c0302bea055ea9763ae7d40d65dabf62
Eric Stanton <Eric@321s-Mac-mini.fios-router.home>
2018-07-17 12:24:54 -0400

    made rooms a class

--- adventure.py
+++ adventure.py
@@ -11,38 +11,7 @@ def direction():
         except (ValueError, TypeError):
             pass
 
-#function for switching rooms
-def move(motion):
-    if motion =="up" and u==True:
-        return motion
-    elif motion =="down" and d==True:
-        return motion
-    elif motion =="left" and l==True:
-        return motion
-    elif motion =="right" and r==True:
-        return motion
-    else:
-        print("you are confused, you walked into a wall, you are not dead yet. -1 Turn.")
-         
-#Owen Wilson or the indian
-def cupboard():
-    return(input("Do you take the \"indian\" or Owen?"))
-
-#wardrobe event
-def wardrobe():
-    while True:
-        r=input("Do you go into the wardrobe? (y/n)")
-        try:
-            if r.lower()=="y" or r.lower()=="n" or r.lower()=="yes" or r.lower()=="no":
-                if r.lower()=="y" or r.lower()=="yes":
-                    print("You enter Narnia. You spend a few hours there and then leave.")
-                    return 2
-                else:
-                    return 0
-        except (TypeError, ValueError):
-            pass
-
-#function to change loc
+#function to change the tuple loc
 def moving(mov):
     y=loc[0]
     x=loc[1]
@@ -60,9 +29,28 @@ def moving(mov):
         x=loc[1]
     return (y,x)
 
+#Owen Wilson or the indian
+def cupboard():
+    return(input("Do you take the \"indian\" or Owen?"))
+
+#wardrobe event
+def wardrobe():
+    while True:
+        r=input("Do you go into the wardrobe? (y/n)")
+        try:
+            if r.lower()=="y" or r.lower()=="n" or r.lower()=="yes" or r.lower()=="no":
+                if r.lower()=="y" or r.lower()=="yes":
+                    print("You enter Narnia. Before you know it you've spent an hour (1 turn) \
+there before leaving.")
+                    return 2
+                else:
+                    return 0
+        except (TypeError, ValueError):
+            pass
+
+#my class I call room to differentiate which room you are in
 class room:
     """This is my attempt to use a class for each room"""
-    __loc=()
     __color=''
     __description=''
     __up=True
@@ -70,8 +58,7 @@ class room:
     __left=True
     __right=True
 
-    def __init__(self,loc,color,description,up,down,left,right):
-        self.__loc=loc
+    def __init__(self,color,description,up,down,left,right):
         self.__color=color
         self.__description=description
         self.__up=up
@@ -79,8 +66,13 @@ class room:
         self.__left=left
         self.__right=right
 
+#returns the color so i can later use it to determine what room they're in
     def get_color(self):
         return self.__color
+
+#prints introduction to  the room
+    def intro(self):
+        print("You are in a room with %s walls." % self.__color)
 
 #hopefully this prints only the correct directions when true.
     def direction(self):
@@ -96,7 +88,33 @@ class room:
             print(" \"right\"", end='')
         print(".")
 
+#prints a room description
+    def info(self):
+        print(self.__description)
 
+#function for switching rooms
+    def move(self,motion):
+        if motion =="up" and self.__up==True:
+            return motion
+        elif motion =="down" and self.__down==True:
+            return motion
+        elif motion =="left" and self.__left==True:
+            return motion
+        elif motion =="right" and self.__right==True:
+            return motion
+        else:
+            print("you are confused, you walked into a wall, you are not dead yet. -1 Turn.")
+
+#setting up the rooms
+white=room("white","""There is an old receptionist desk in the middle of the room with nothing
+of value on it or in it.""",True,False,True,True)
+pink=room("pink","There is a cupboard that perks your interest.",False,False,False,True)
+purple=room("purple","There is a wardrobe with the door cracked open that peaks your interest.",\
+False,False,True,False)
+green=room("green","There is an exit sign above a keyhole in the \"up\" direction.",False,True,\
+False,True)
+orange=room("orange","There is nothing but a miniature mouse hole in the wall.",False,False,\
+True,False)
 
 #set-up
 temp=[]
@@ -120,7 +138,7 @@ enough oxygen to enter 10 rooms until you pass out.""")
         loc=(1,5)
         mini=5
         key = False
-    print (loc)
+#print (loc) I used while testing
 
 #if you run out of time
     if turn==10:
@@ -136,109 +154,81 @@ enough oxygen to enter 10 rooms until you pass out.""")
             break
             
      
-#first room
+#checks what room you're in
     if loc == (1,5):
-        u=True
-        l=True
-        r=True
-        d=False
-        turn+=1
-        print("You are in a room with white walls.")
-        print("There are doors \"up\", \"left\",and \"right\".")
-        print("""There is an old receptionist desk in the middle of the room with nothing
-of value on it or in it.""")
-        hi=direction()
-        yo=move(hi)
-        temp=moving(yo)
-        loc=temp
-       
-    elif loc == (1,4):
-        r=True
-        u=False
-        l=False
-        d=False
-        turn+=1
-        print("You enter a room with pink walls.")
-        print("There is a door only on the \"right\".")
-        if mini==5:
-            print("""There is a cupboard that perks your interest. Little people appear to be fighting.
-You notice one of them is Owen Wilson and the other is just some indian. You don't want
-them to fight anymore so you take one of them. Which do you want to take?""")
-            if cupboard().lower() == "indian":
-                print("You grab the indian.")
-                mini=1
-            else:
-                print("Owen Wilson jumps into your hand.")
-                mini=2
-        hi=direction()
-        yo=move(hi)
-        temp=moving(yo)
-        loc=temp
+        location=white
+
+    elif loc==(1,4):
+        location=pink
 
     elif loc == (1,6):
-        l=True
-        u=False
-        r=False
-        d=False
-        turn+=1
-        print("You enter a room with purple walls.")
-        print("There is a door only on the \"left\".")
-        print("There is a wardrobe with the door cracked open that peaks your interest.")
-        turn+=wardrobe()
-        hi=direction()
-        yo=move(hi)
-        temp=moving(yo)
-        loc=temp
+        location=purple
 
     elif loc == (2,5):
-        l=False
-        u=False
-        d=True
-        r=True
-        turn+=1
-        print("You enter a room with green walls.")
-        print("There are doors \"up\", \"down\", and \"right\".")
-        print("There is an exit sign above the door in the \"up\" direction.")
-        hi=direction()
-        if hi =="up":
-            if key==True:
-                print ("congratulations you didn't die!")
-                break
-            else:
-                if mini==1:
-                    print("""you try to open the door. It is locked. You try to knock
-    it down by charging at it and fall. The indian is not amused.""")
-                elif mini==2:
-                    print("you try to open the door. It is locked. Owen Wilson laughs.")
-                else:
-                    print("You try to open the door, but it is locked.")
-        else:
-            yo=move(hi)
-            temp=moving(yo)
-            loc=temp
+        location=green
 
     elif loc == (2,6):
-        l=True
-        u=False
-        d=False
-        r=False
-        turn+=1
-        print("You enter an orange room.")
-        print("There is only a door on the \"left\".")
-        print("""There is nothing but a miniature mouse hole in the wall.
-If only you had a little person to go investigate the hole.""")
-        if key==False:
+        location=orange
+
+#prints the color of the room 
+    location.intro()
+
+#prints a room description
+    location.info()
+    turn+=1
+
+#taking a miniature person is a forced result of entering the pink room
+    if location.get_color()=="pink" and mini==5:
+        print("""Miniature people appear to be fighting in the cupboard. You notice one
+of them looks like Owen Wilson and the other is just some indian. You don't want them
+to fight anymore so you take one of them. Which do you want to take?""")
+        if cupboard().lower() == "indian":
+            print("You grab the indian.")
+            mini=1
+        else:
+            print("Owen Wilson jumps into your hand.")
+            mini=2
+
+#retrieving the key if in the orange room                
+    if location.get_color()=="orange" and key==False:
             if mini==1:
-                print("The indian jumps down and into the hole and emerges with a key shortly after.")
+                print("The indian jumps down without hesitation into the hole and emerges with \
+a key shortly after.")
                 key=True
             elif mini==2:
                 key=True
                 print("""Owen Wilson looks at the hole. He is clearly terrified. After a long while
 and some encouragement from you he goes into the hole. After some screaming and
 a panic attack he emerges with a key.""")
-                turn+=1
-        hi=direction()
-        yo=move(hi)
-        temp=moving(yo)
-        loc=temp
+            else:
+                print("If only you had a miniature person to investigate the hole.")
+    
+#purple room asks if you want to go into the wardrobe, if you do waste a turn.
+    if location.get_color()=="purple":
+        turn+=wardrobe()
 
+#prints which directions are available based on what room you are in
+    location.direction()
+
+#asks user what direction they want to go and checks for valid input
+    hi=direction()
+
+#if you choose up in the green room it checks if you have the key for victory.
+    if location.get_color()=="green" and hi =="up":
+        if key==True:
+            print ("The wall seperates in front of you. Congratulations you have escaped!")
+            break
+        else:
+            print("""You inspect the keyhole. It looks like a regular keyhole only a bit smaller.
+Upon closer investigation there is a slight vertical line from floor to ceiling just beside it.
+You charge into the wall and fail to break it open. You need to find a key.""")
+            if mini==1:
+                print("The indian is not amused.")
+            elif mini==2:
+                print("Owen Wilson laughs.")
+    
+#moves the player checking what room they are in and only advances if ther eis a door there
+    yo=location.move(hi)
+    temp=moving(yo)
+    loc=temp
+       
--- test.py
+++ test.py
@@ -8,5 +8,31 @@ h=[]
 h=["1","1","1"]
 print (len(h))
 
+class room:
+    name=''
+
+    def __init__(self,name):
+        self.name=name
+
+    def get_name(self):
+	    return self.name
+
+    def intro(self):
+        print("You are in a room with %s walls." % self.name)
+
+    def info(self):
+        print(self.name)
+a="a"
+b="b"
+if a>b:
+    print("hi")
+if b>a:
+    print("ho")
+    
+red=room("red")
+test=red
+print(red.get_name())
+test.intro()
+test.info()
 #git add .
 #git commit -m “testing”
